1122dbb5f2a35cf296e4e6c7ef920634a169bf7d
Gabz Holanda <gabrielholallan09@gmail.com>
2025-06-09 21:49:23 -0300

    Update soma.py

--- soma.py
+++ soma.py
@@ -1,2 +1,14 @@
-def soma(e,b):
-    return e+b
+import os
+import math
+def soma(X,Y):
+    return X + Y
+def sub(X,Y):
+    return X - Y
+def multi(X,Y):
+    return X * Y
+def Div(X,Y):
+    return X / Y
+def raiz(X):
+    return math.sqrt(X)
+def limpa():
+    return os.system('cls')
